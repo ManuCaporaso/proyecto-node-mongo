74d19e2bb027e040ed6405c5971f835dce1f4022
Manu Caporaso <manulpda@gmail.com>
2025-09-05 15:51:35 -0300

    conectando a DB

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-//router.get('/', (req, res) => {  res.send('¡Hola, mundo! Esta es la ruta raíz.');}); 
+router.get('/', (req, res) => {  res.send('¡Hola, mundo! Esta es la ruta raíz.');}); 
 
 router.use('/users', require('./user'));
 
